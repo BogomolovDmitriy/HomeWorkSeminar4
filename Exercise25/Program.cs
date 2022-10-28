e060efaf2cb95083ab30d9004047a6d6e7263ac5
Dmitriy <dimgod80@yandex.ru>
2022-10-28 19:57:25 +0800

    add Exercise25

---
+++ Exercise25/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 25: 
+// Напишите (функцию) цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+double DegreeNumber(int num1,int num2)
+{
+    double result = Math.Pow(num1, num2);
+    Console.WriteLine($"Рузультат: {result}");
+    return result;
+}
+
+Console.Write("Введите число, которое нужно возвести в степень: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите степень, для указанного числа: ");
+int b = int.Parse(Console.ReadLine());
+
+DegreeNumber(a, b);
